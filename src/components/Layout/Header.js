f80c4e27fe09747cdfbbc6cd213b83fab065da12
kamath7 <adithyakamath96@gmail.com>
2021-07-10 00:04:05 +0530

    Added Cart icon

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import classes from './Header.module.css'
 import mealsImage from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
 const Header = () => {
     return (
         <React.Fragment>
             <header className={classes.header}>
                 <h1>Meal Master</h1>
-                <button>Your Cart</button>
+                <HeaderCartButton/>
             </header>
             <div>
                 <img src={mealsImage} className={classes['main-image']} alt={"Some dude's table"}/>
