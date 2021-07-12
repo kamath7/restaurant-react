57a03e91b36ca4d087dc9e8c2285b5b7e16f9f98
kamath7 <adithyakamath96@gmail.com>
2021-07-13 01:22:02 +0530

    Added cart item length from Context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,21 @@
-import React , {useContext} from 'react';
-import CartIcon from '../Cart/CartIcon';
-import CartContext from '../../store/CartContext';
-import classes from './HeaderCartButton.module.css';
+import React, { useContext } from "react";
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/CartContext";
+import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
-	return (
-		<button className={classes.button} onClick={props.onClick}>
-			<span className={classes.icon}>
-				<CartIcon />
-			</span>
-			<span>Your Cart</span>
-			<span className={classes.badge}>3</span>
-		</button>
-	);
+  const cartCtx = useContext(CartContext);
+  const itemsNumber = cartCtx.items.reduce((current, item) => {
+    return current + item.amount;
+  }, 0);
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{itemsNumber}</span>
+    </button>
+  );
 };
 
 export default HeaderCartButton;
