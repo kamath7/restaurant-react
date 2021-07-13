3eee9b9114675c76b6504547c8d81784be654757
kamath7 <adithyakamath96@gmail.com>
2021-07-13 15:51:25 +0530

    Reducer implemented

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,11 +1,35 @@
+import React, { useReducer } from "react";
 import CartContext from "./CartContext";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      const updatedItems = state.items.concat(action.payload);
+      const updatedTotalAmt =
+        state.totalAmount + action.payload.price * action.payload.amount;
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmt,
+      };
+      break;
+  }
+  return defaultCartState;
+};
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+  const [cartState, dispatch] = useReducer(cartReducer, defaultCartState);
+  const addItemToCartHandler = (item) => {
+    dispatch({ type: "ADD_ITEM", payload: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatch({ type: "REMOVE_ITEM", payload: id });
+  };
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
