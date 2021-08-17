6c2fe96f65db47404106527abc1eea687a68dd24
kamath7 <adithyakamath96@gmail.com>
2021-08-18 02:02:32 +0530

    styling added

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,6 +1,9 @@
 import React from "react";
 import classes from "./Checkout.module.css";
 const Checkout = (props) => {
+  const confirmHandler = (event) => {
+    event.preventDefault();
+  };
   return (
     <form>
       <div className={classes.control}>
@@ -20,8 +23,12 @@ const Checkout = (props) => {
         <label htmlFor="city">City </label>
         <input type="text" id="city" />
       </div>
-      <button type="button" onClick={props.onCancel}>Cancel</button>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+      <button type="button" onClick={props.onCancel}>
+        Cancel
+      </button>
+      <button className={classes.submit} onClick={confirmHandler}>Confirm</button>
+      </div>
     </form>
   );
 };
